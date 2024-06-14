eb4be90e84b5bcc86829c572ee1ca4919646bf19
zunaid <zunaidhossain39@gmail.com>
2024-06-14 16:34:29 +0600

    simple_login

--- first/forms.py
+++ first/forms.py
@@ -6,4 +6,4 @@ from django.contrib.auth.forms import UserCreationForm
 class registrationForm(UserCreationForm):   
     class Meta:
        model=User
-       fields=['username','first_name','last_name','email', ]
+       fields=['username','first_name','last_name','email']
